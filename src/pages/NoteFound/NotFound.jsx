ca2467b09d1695679735dd4e9cbf76e49b6b8c23
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-05-05 16:50:19 +0200

    update not Found
    css+ img

--- src/pages/NoteFound/NotFound.jsx
+++ src/pages/NoteFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import { GoHome } from "../../components/GoHome/GoHome";
+import styles from "../Home/HomePage.module.css"
 
 
 export const NotFound = () => {
   return (
-    <>
-      <h1>Page Not Found</h1>
+    <div className={styles.notFound}>
+      <img src="https://cdn.pixabay.com/photo/2021/07/21/12/49/error-6482984_1280.png" alt="not found" width = "80%" height="60%" />
       <GoHome />
-    </>
+    </div>
   );
 };
